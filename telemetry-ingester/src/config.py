78170fc37776e327c870dc6284c8977877f2aab0
Alessandro Aimi <alleaimi95@gmail.com>
2025-08-17 21:39:39 +0200

    introduce telemetry-ingester

---
+++ telemetry-ingester/src/config.py
@@ -0,0 +1,21 @@
+import toml
+
+
+class Config:
+    def load(self, path: str):
+        c = toml.load(path + "/config.toml")
+
+        mosquitto = c["mosquitto"]
+        self.MOSQUITTO_HOSTNAME = mosquitto["endpoint"].split(":")[0]
+        self.MOSQUITTO_PORT = int(mosquitto["endpoint"].split(":")[1])
+        self.MOSQUITTO_TOPICS = mosquitto["topics"]
+        self.MOSQUITTO_QOS = mosquitto["qos"]
+
+        influxdb = c["influxdb"]
+        self.INFLUXDB_URL = "http://" + influxdb["endpoint"]
+        self.INFLUXDB_TOKEN = influxdb["token"]
+        self.INFLUXDB_ORG = influxdb["org"]
+        self.INFLUXDB_BUCKET = influxdb["bucket"]
+
+
+settings = Config()
---
+++ telemetry-ingester/src/events.py
@@ -0,0 +1,23 @@
+from google.protobuf.json_format import MessageToDict
+from chirpstack_api import integration
+
+CHIRPSTACK_EVENTS = {
+    "up": integration.UplinkEvent,
+    "status": integration.StatusEvent,
+    "join": integration.JoinEvent,
+    "ack": integration.AckEvent,
+    "txack": integration.TxAckEvent,
+    "log": integration.LogEvent,
+    "location": integration.LocationEvent,
+    "integration": integration.IntegrationEvent,
+}
+
+
+def unmarshal_event_to_dict(payload: bytes, event: str) -> dict:
+    protobuf_message = CHIRPSTACK_EVENTS[event]()
+    protobuf_message.ParseFromString(payload)
+    return MessageToDict(
+        protobuf_message,
+        always_print_fields_with_no_presence=True,
+        preserving_proto_field_name=True,
+    )
