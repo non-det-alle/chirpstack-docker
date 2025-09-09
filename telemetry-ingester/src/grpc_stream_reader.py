593fd02d628378601779677c4865a73bd1c2f0b2
Alessandro Aimi <alleaimi95@gmail.com>
2025-09-09 14:38:25 +0200

    telemetry-ingester: improve gRPC reader forever loop

--- telemetry-ingester/src/grpc_stream_reader.py
+++ telemetry-ingester/src/grpc_stream_reader.py
@@ -9,14 +9,6 @@ from .config import settings
 from .logger import logger
 
 
-def message_to_dict(message: Message) -> dict:
-    return MessageToDict(
-        message,
-        always_print_fields_with_no_presence=True,
-        preserving_proto_field_name=True,
-    )
-
-
 class BaseGRPCStreamReader:
     def __init__(self, on_read):
         self._endpoint = settings.CHIRPSTACK_ENDPOINT
@@ -25,7 +17,6 @@ class BaseGRPCStreamReader:
         self._channel = grpc.aio.insecure_channel(self._endpoint)
         self._metadata = [("authorization", f"Bearer {self._token}")]
         self._on_read = on_read
-        self._tracking = {}  # dev_eui -> (last seen, count)
 
         self._internal_api = chirpstack_api.InternalServiceStub(self._channel)
 
@@ -38,41 +29,52 @@ class BaseGRPCStreamReader:
     async def close(self):
         await self._channel.close()
 
-    async def _read_forever(self, id, stream):
-        self._tracking[id] = (datetime.now(timezone.utc), 0)
+    async def _read_forever(self, stream):
+        async def read_stream():
+            return MessageToDict(
+                await stream.read(),
+                always_print_fields_with_no_presence=True,
+                preserving_proto_field_name=True,
+            )
 
-        def obsolete():
-            last_seen, count = self._tracking[id]
-            now, count = datetime.now(timezone.utc), count + 1
-            msg_time = datetime.fromisoformat(log_item["time"])
-            assert now > msg_time  # otherwise timezone issues?
-            msg_age = now - msg_time
-            log_args = (id, count, now - last_seen, msg_age)
-            logger.debug("{}: count={}, prev={}, age={}".format(*log_args))
-            self._tracking[id] = (now, count)
-            return msg_age > timedelta(seconds=1)
+        async def first_message():
+            # on stream instantiation, chirpstack always fetches up to 10
+            # logs (as this was meant for UI display) but we want the actual
+            # latest log(s) that triggered the server-side send
 
-        while True:
-            try:
-                message = await stream.read()
-            except grpc.aio.AioRpcError as e:
-                logger.error(f"gRPC error: {e.details()}")
-                self._tracking.pop(id, None)
-                break
-            log_item = message_to_dict(message)
-            if not obsolete():
+            def message_is_obsolete():
+                msg_time = datetime.fromisoformat(message["time"])
+                now = datetime.now(timezone.utc)
+                assert now > msg_time  # otherwise timezone issues?
+                msg_age = now - msg_time
+                logger.debug(f"msg_age={msg_age}")
+                # chirpstack checks for new logs every 1 second
+                return msg_age > timedelta(seconds=1)
+
+            # consume out-of-date logs
+            message = await read_stream()
+            while message_is_obsolete():
+                message = await read_stream()
+            return message
+
+        try:
+            log_item = await first_message()
+            while True:
                 await self._on_read(log_item)
+                log_item = await read_stream()
+        except grpc.aio.AioRpcError as e:
+            logger.error(f"gRPC error: {e.details()}")
 
 
 class GRPCDeviceFramesReader(BaseGRPCStreamReader):
     async def read(self, dev_eui):
         req = chirpstack_api.StreamDeviceFramesRequest(dev_eui=dev_eui)
         stream = self._internal_api.StreamDeviceFrames(req, metadata=self._metadata)
-        await self._read_forever(dev_eui, stream)
+        await self._read_forever(stream)
 
 
 class GRPCDeviceEventsReader(BaseGRPCStreamReader):
     async def read(self, dev_eui):
         req = chirpstack_api.StreamDeviceEventsRequest(dev_eui=dev_eui)
         stream = self._internal_api.StreamDeviceEvents(req, metadata=self._metadata)
-        await self._read_forever(dev_eui, stream)
+        await self._read_forever(stream)
